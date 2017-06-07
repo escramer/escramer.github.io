b350d0623fc0956078eea9ae5d9529f2300da99a
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-06 17:01:30 -0700

    Now have a background

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-ctx = document.getElementById('canvas');
+function main() {
+  requestAnimationFrame(main);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Background
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext('2d');
+requestAnimationFrame(main);
