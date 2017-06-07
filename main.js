fb773184a03a76c77ac83a049535d2357e51e27b
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-06 18:52:35 -0700

    Made the skeleton structure for main.js

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-function main() {
-  requestAnimationFrame(main);
+function World() {
+  this.update = function() {
+    ; //todo
+  }
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Background
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  this.draw = function() {
+    ; //todo
+  }
 }
 
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-canvas.width = consts.screenW;
-canvas.height = consts.screenH;
-requestAnimationFrame(main);
+// Do initializations here.
+function init() {
+  var canvas = document.getElementById('myCanvas');
+
+  canvas.width = consts.screenW;
+  canvas.height = consts.screenH;
+  window.canvas = canvas;
+  window.ctx = canvas.getContext('2d');
+  window.world = World();
+}
+
+// Update the state of the world, draw it, and repeat.
+function mainLoop() {
+  requestAnimationFrame(mainLoop);
+  world.update();
+  world.draw();
+}
+
+init();
+requestAnimationFrame(mainLoop);
