8631fba22ea67e4328cb61910d99e1698b3b181f
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-06 18:12:28 -0700

    Added consts.js

---
+++ consts.js
@@ -0,0 +1,11 @@
+var consts = {
+  ballSpeed: 30,
+  ballR: 5,
+  maxBallAngle: 75 * Math.PI / 180
+  paddleW: 2,
+  paddleH: 28,
+  paddlePad: 10,
+  paddleSpeed: 256,
+  screenH: 256,
+  screenW: 512
+}
