392ef43fbfd108c4cc9bce0d659afce9ebd8b3ee
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-06 18:14:52 -0700

    Added color to consts

--- consts.js
+++ consts.js
@@ -1,11 +1,12 @@
 var consts = {
   ballSpeed: 30,
   ballR: 5,
-  maxBallAngle: 75 * Math.PI / 180
+  maxBallAngle: 75 * Math.PI / 180,
   paddleW: 2,
   paddleH: 28,
-  paddlePad: 10,
+  paddlePad: 10, // Distance from edge
   paddleSpeed: 256,
   screenH: 256,
-  screenW: 512
+  screenW: 512,
+  color: 'white'
 }
