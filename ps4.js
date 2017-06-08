2cc01244703a0ed8ca91cedb3ee67485fe7bfc57
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-07 22:44:52 -0700

    Added ps4.js

---
+++ ps4.js
@@ -0,0 +1,43 @@
+var ps4 = {
+  X: 0,
+  CIRCLE: 1,
+  SQUARE: 2,
+  TRI: 3,
+  L1: 4,
+  R1: 5,
+  L2: 6,
+  R2: 7,
+  SHARE: 8,
+  OPTIONS: 9,
+  L3: 10,
+  R3: 11,
+  UP: 12,
+  DOWN: 13,
+  LEFT: 14,
+  RIGHT: 15,
+  PS: 16,
+  TOUCH: 17,
+
+  // Log the buttons being pressed.
+  _showButtons: function() {
+    var gamepad = navigator.getGamepads()[0]
+    var buttons;
+    if (gamepad === null) {
+      console.log('Controller disconnected');
+    }
+    else {
+      console.log('Below are the buttons currently pressed:');
+      buttons = gamepad.buttons;
+      for (var ndx = 0; ndx < buttons.length; ndx++) {
+        if (buttons[ndx].pressed) {
+          console.log(ndx);
+        }
+      }
+    }
+  },
+
+  // Log the buttons being pressed every second.
+  test: function() {
+    setInterval(this._showButtons, 1000);
+  }
+}
