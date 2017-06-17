bead3e3522a5f5d00405b03d6735943211953aa3
Eric Cramer <ericcramer@Eric-Cramers-MacBook.local>
2017-06-16 22:40:31 -0700

    Made a stub for the neural network

--- nn.js
+++ nn.js
@@ -1,1 +1,23 @@
 // Neural network
+
+/* A neural network.
+
+hiddenUnits can either be an integer or an array. If it's an integer, have one
+layer with that many hidden units. If it's an array, the ith element represents
+the number of hidden units in the ith hidden layer.
+*/
+function NeuralNetwork(numInputs, numOutputs, hiddenUnits=100) {
+  /* Fit this neural network to this single training example.
+
+  input is a one-dimensional matrix. output is an integer from 0 to (numOutputs
+  - 1).
+  */
+  this.fit = function(input, output) {
+    ;//todo
+  }
+
+  // Return the output. input is a one-dimensional matrix.
+  this.predict = function(input) {
+    return 0; //todo
+  }
+}
